500f009ed6b3eccc86cc0204020e74141666d0f8
Daniel Rodriguez <danrod@gmail.com>
2024-06-20 23:21:57 -0400

    Changes in fetch() call to add basic authentication

--- js/h5p-xapi.js
+++ js/h5p-xapi.js
@@ -1,24 +1,48 @@
 /**
  * @file
- * Default JavaScript file for Block Class.
+ * Default JavaScript file for H5P XAPI module.
  */
 
 (function ($, Drupal, drupalSettings) {
   'use strict';
 
-  Drupal.behaviors.blockClass = {
+  Drupal.behaviors.h5pXapi = {
     attach: function (context, settings) {
-      const elements = once('wrapped', '.h5p-iframe-wrapper', context);
+      const elements = once('h5pxapiLibrary', '.h5p-iframe-wrapper', context);
 
       elements.forEach(function(index){
-        console.log("Test Load");
         let userId = drupalSettings.h5pxapi.userId;
         let nodeId = drupalSettings.h5pxapi.nodeId;
 
-        console.log(userId);
-        console.log(nodeId);
-
         H5P.externalDispatcher.on('xAPI', function (event) {
+          /* TODO
+           *
+           * - make a fetch call to send a POST request to /h5p-xapi/save-events-user
+           * - check for errors and other exceptions 
+           * - OPTIONAL: to parse the event.data.statement object before sending it */
+          let data = {
+            user_id: userId,
+            node_id: nodeId,
+            h5p_event: event.data.statement
+          };
+
+          /* TODO
+           * 
+           * - Check for errors, success values, and return values also
+           * - Make a fetch() call to /session/token' save the return value somehow 
+           * - use that value in the X-Csrf-Token header value
+           * - Get session cookie (somehow)
+           * - Change "Authorization" to "Cookie" */
+          fetch("/h5p-xapi/save-events-user?_format=json", {
+            body: JSON.stringify(data),
+            headers: {
+              "Authorization": "Basic ZGFucm9kdGVzdDp0ZXN0MTE=",
+              "Content-Type": "application/json",
+              "X-Csrf-Token": "QqwBBvo2rvacoaJjSGOxrfjklStTw2wqLMsYgHkaetI"
+            },
+            method: "POST"
+          });
+
           console.log(event.data.statement);
         });
       });
